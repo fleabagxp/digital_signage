cc1be152939b5da87414a001d719ff1f396971ff
Guypetch Mamorchok <fleabagxp@gmail.com>
2019-03-06 09:52:35 +0700

    add video

--- resources/views/display-front.blade.php
+++ resources/views/display-front.blade.php
@@ -14,10 +14,14 @@
 				width: 100%;
 				height: auto;
 			}
+			video {
+				width: 100%;
+				height: auto;
+			}
 			.top {
 				margin: 0;
 				padding: 0;
-				margin-top: 24%;
+				margin-top: 36%;
 			}
 			.bottom {
 				margin: 0;
@@ -27,23 +31,23 @@
 <body>
 	<div class="container-fluid">
 		<div class="row">
-			@foreach($displayImages as $displayImage)
-				<div class="col-md-12 top">
-					@if($displayImage->position === 'signage1')
-						<img src="{{ $displayImage->path }}">
-					@endif
-				</div>
+			<div class="col-md-12 top">
+				@if(isset($displayImages->where('position', 'signage1')->first()->position) && $displayImages->where('position', 'signage1')->first()->position === 'signage1')
+				<video controls autoplay loop>
+					<source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+				</video>
+				@elseif(isset($displayImages->where('position', 'signage1')->first()->position) && $displayImages->where('position', 'signage1')->first()->position === 'signage1')
+					<img src="{{ $displayImages->where('position', 'signage1')->first()->path }}">
+				@endif
+			</div>
 				
-				<div class="col-md-12 fixed-bottom bottom">
-					@if($displayImage->position === 'signage2')
-						<img src="{{ $displayImage->path }}">
-					@endif
-				</div>
-				
-			@endforeach
+			<div class="col-md-12 fixed-bottom bottom">
+				@if(isset($displayImages->where('position', 'signage1')->first()->position) && $displayImages->where('position', 'signage2')->first()->position === 'signage2')
+					<img src="{{ $displayImages->where('position', 'signage2')->first()->path }}">
+				@endif
+			</div>
 		</div>
 	</div>
-	
 </body>
 </head>
 </html>
